afbf76a5bfbf8ef81c6586759b03e1ea6997128a
bigHead <87469737+ylxylxylx@users.noreply.github.com>
2023-08-30 11:04:10 +0800

    Create vite.config.ts

---
+++ v3-template/vite.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
